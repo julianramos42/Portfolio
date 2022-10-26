6474986d5b260289ef2ec7b2b2eea401965df7ef
Julián Ramos <99379303+julianramos42@users.noreply.github.com>
2022-10-26 09:55:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let mostrarMas = document.getElementById('mostrarMas');
+let hideImg = document.getElementById('hideImg');
+
+mostrarMas.addEventListener('click',mostrarImagenes);
+
+function mostrarImagenes(){
+    hideImg.classList.toggle('show');
+    if(hideImg.classList.contains('show')){
+        mostrarMas.innerHTML='Ver Menos';
+    }else{
+        mostrarMas.innerHTML='Ver Más';
+    }
+}
+
+const imageWrapper = document.querySelector('.image-wrapper')
+const imageItems = document.querySelectorAll('.image-wrapper > *')
+const imageLength = imageItems.length
+const perView = 3
+let totalScroll = 0
+const delay = 2000
+
+imageWrapper.style.setProperty('--per-view', perView)
+for(let i = 0; i < perView; i++) {
+  imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
+}
+
+let autoScroll = setInterval(scrolling, delay)
+
+function scrolling() {
+  totalScroll++
+  if(totalScroll == imageLength + 1) {
+    clearInterval(autoScroll)
+    totalScroll = 1
+    imageWrapper.style.transition = '0s'
+    imageWrapper.style.left = '0'
+    autoScroll = setInterval(scrolling, delay)
+  }
+  const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
+  imageWrapper.style.left = `-${totalScroll * widthEl}px`
+  imageWrapper.style.transition = '.3s'
+}
